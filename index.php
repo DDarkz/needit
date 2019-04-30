6f051fe7d5207ac7f9d277d71dd93ed9ae70d11f
Sofiane Boubrit <Sofiane.Boubrit@gmail.com>
2019-04-30 18:45:22 -0400

    Changement des titre

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Nom de site</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Need it now</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -53,10 +53,8 @@
   <header class="masthead">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 text-uppercase">Nom de site</h1>
-        <h2 class="text-white-50 mx-auto mt-2 mb-5">Rendez Service<br> 
-          et<br> 
-          gagner des bénéfices<br>
+        <h1 class="mx-auto my-0 text-uppercase">NEED IT NOW</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">Rendez Service et gagner des bénéfices<br>
           <p class="text-white-50">aidez les gens et gagner de l'argent</p></h2>
         <a href="annonces.php" class="btn btn-primary js-scroll-trigger">Get Started</a>
       </div>
