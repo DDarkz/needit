0f9487cad03399e58dddb487320344220e9f3507
mjoly75 <britanicus@hotmail.com>
2019-04-30 19:37:25 -0400

    changer bdname

--- bd/connexion.php
+++ bd/connexion.php
@@ -4,11 +4,11 @@ define("USAGER", "root");
 define("PASSE", "");
 
 try {
-	$dns = 'mysql:host=localhost;dbname=bdusers;charset=utf8';
+	$dns = 'mysql:host=localhost;dbname=bdservice;charset=utf8';
 	$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 	$connexion = new PDO($dns, USAGER, PASSE, $options);
 } catch (Exception $e) {
-	echo "Erreur de connexion à la base de donnée bdusers.";
+	echo "Erreur de connexion à la base de donnée bdservice.";
 	echo $e->getMessage();
 	exit();
 };
