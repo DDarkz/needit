bc92820e63808a05b6b9d85d11488a05fac41ad8
EmnaKhelifi <Emna.kh@gmail.com>
2019-05-03 13:41:49 -0400

    rajouter bouton profil et page profil utilisateru test

---
+++ profilUtilisateur.php
@@ -0,0 +1,34 @@
+<?php
+include("bd/connexion.php");
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="vues/vues.js"></script>
+    <script type="text/javascript" src="requetes/requetes.js"></script>
+    <title>Profil</title>
+  </head>
+  <body>
+  <!-- debut container -->
+  <div class="container pt-5">
+       <h1>Bonjour Bienvenue chez Need IT NOW !!</h1>
+       <h2>Votre profil</h2>
+
+    <?php include("includes/menu.php"); ?>
+   
+
+    <!-- debut container -->
+</div>
+<!-- fin container -->
+
+<?php include("includes/footer.php"); ?>
+    <?php include("includes/footer-script.php"); ?>
+  </body>
+</html>
