43d3b510cd72addd83d75004b4d782d2597906e7
Sofiane Boubrit <Sofiane.Boubrit@gmail.com>
2019-05-14 01:28:38 -0400

    petite correction dans le chat

--- ajax/recup.php
+++ ajax/recup.php
@@ -13,19 +13,34 @@
     $demandeur = $row['courriel'];
     $livreur = $_SESSION['courriel'];
 
-    $req = $connexion->query("SELECT * FROM messages WHERE 
-       (livreur='$livreur' AND demandeur='$demandeur' AND idAnnonce = '$idAnnonce') 
-    OR (demandeur='$livreur' AND livreur='$demandeur'AND idAnnonce = '$idAnnonce') 
-    OR (livreur='$demandeur' AND demandeur='$demandeur'AND idAnnonce = '$idAnnonce')
-    OR (idAnnonce = '$idAnnonce')");
-    $results = array();
+    $reqDemandeur = "SELECT idUser FROM connexion WHERE `courriel` = '$livreur'";
+    foreach($connexion->query($reqDemandeur) as $rowL)
+    $idLivreur = $rowL['idUser'];
 
-    while($rows = $req->fetchObject()){
-        $results[] = $rows;
+
+    if($idDemandeur == $idLivreur){
+        $req = $connexion->query("SELECT * FROM messages WHERE 
+        (livreur='$livreur' AND demandeur='$demandeur' AND idAnnonce = '$idAnnonce') 
+        OR (demandeur='$livreur' AND livreur='$demandeur'AND idAnnonce = '$idAnnonce') 
+        OR (livreur='$demandeur' AND demandeur='$demandeur'AND idAnnonce = '$idAnnonce')
+        OR (idAnnonce = '$idAnnonce')");
+        $results = array();
+
+        while($rows = $req->fetchObject()){
+            $results[] = $rows;
+        }
+    }else{
+        $req = $connexion->query("SELECT * FROM messages WHERE livreur='$livreur' AND idAnnonce = '$idAnnonce'");
+        $results = array();
+
+        while($rows = $req->fetchObject()){
+            $results[] = $rows;
+        }
     }
 
     foreach($results as $message){
         ?>
+            <p class="card-text"><small class="text-muted" value="<?=$idLivreur?>"></small></p>
             <div class="message <?php echo ($message->livreur == $livreur) ? 'message-Livreur' : 'message-demandeur' ?>">
                 <?= $message->message ?>
 
