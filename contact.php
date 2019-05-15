4cb1b4223b28aa5c519d480256fff24cc1dc685a
EmnaKhelifi <Emna.kh@gmail.com>
2019-05-15 13:33:38 -0400

    ajout contenu contact

--- contact.php
+++ contact.php
@@ -1,34 +1,222 @@
-<?php
+   
+  <?php
 include("bd/connexion.php");
 session_start();
-
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <script type="text/javascript" src="vues/vues.js"></script>
-    <script type="text/javascript" src="requetes/requetes.js"></script>  
+<head>
 
-    <title>Toutes les annonces</title>
-  </head>
-  <body onload="requetes(null,'actionLister');">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <?php include("includes/menu.php"); ?>
+  <title>Contact</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <script type="text/javascript" src="vues/vues.js"></script>
+  <script type="text/javascript" src="requetes/requetes.js"></script>  
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/grayscale.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="index.php"><img src="images\logoGoFor.png" width="100"></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" class="dropdown-item active" href="propos.php">Propos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="annonces.php">Annonces</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="contact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="description.php">Comment ça marche</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h1 class="mx-auto my-0 text-uppercase">Contactez nous</h1>
+        <!-- <h2 class="text-white-50 mx-auto mt-2 mb-5">Rendez Service et gagner des bénéfices<br>
+          <p class="text-white-50">aidez les gens et gagner de l'argent</p></h2>
+        <a href="annonces.php" class="btn btn-primary js-scroll-trigger">Commencez</a> -->
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section id="about" class="about-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2 class="text-white mb-4">Ecrivez nous !</h2>
+          <p class="text-white-50"></p>
+          <form> 
+          <div class="form-group row">
+            <label for="nom" class="text-white-50 col-sm-2 col-form-label">Nom</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="nom" name="nom" Required>
+            </div>
+           
+          </div>
+          <div class="form-group row">
+            <label for="prenom" class="text-white-50 col-sm-2 col-form-label">Prénom</label>
+            <div class="col-sm-10">
+              <input type="text"  class="form-control" id="prenom" name="prenom"  Required >
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="courriel" class="col-sm-2 col-form-label">Courriel</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="courriel" name="courriel" aria-describedby="emailHelp"  Required>
+            </div> 
+            
+          </div>
+          <div class="form-group row">
+            <label for="objet" class="text-white-50 col-sm-2 col-form-label">Objet</label>
+            <div class="col-sm-10">
+              <input type="text"  class="form-control" id="objet" name="objet"  Required >
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="sujet" class="text-white-50 col-sm-2 col-form-label">Sujet</label>
+            <div class="col-sm-10">
+              <textarea rows="4" cols="50" class="form-control" ></textarea>
+            </div>
+          </div>
+           <a href="formulaireLogin.php" class="btn btn-primary js-scroll-trigger form-control ">Contacter</a><br>
+          </form><br>
+           <!-- <p> <a href="formulaireLogin.php" class="btn btn-primary js-scroll-trigger">Se connecter</a>
+           <a href="formulaireInscription.php"><br>Pas encore inscrit? </a></p> -->
+        </div>
+      </div>
+      <!-- <img src="images/Gofor.jpg" class="img-fluid" alt=""> -->
+    </div>
+  </section>
+
+  <!-- Projects Section -->
+  <section id="projects" class="projects-section bg-light">
+    <div class="container" id="annoncesIndex">
 
 
 
+    </div>
+  </section>
+<!-- 
+  Signup Section
+  <section id="signup" class="signup-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto text-center">
 
+          
 
+        </div>
+      </div>
+    </div>
+  </section> -->
 
-     <!-- fin container -->
+  <!-- Contact Section -->
+  <section class="contact-section bg-black">
+    <div class="container">
 
-     <?php include("includes/footer.php"); ?>
-    <?php include("includes/footer-script.php"); ?>
+      <div class="row">
+
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Adresse</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">9155 Rue St-Hubert, Montréal, QC H2M 1Y8</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-envelope text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Email</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">
+                <a href="#">Gofor@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-mobile-alt text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Phone</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">+1 (555) 902-8832</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="social d-flex justify-content-center">
+        <a href="#" class="mx-2">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#" class="mx-2">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="#" class="mx-2">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; GoFor 2019
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
