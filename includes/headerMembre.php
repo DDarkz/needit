00345972935f71a33a153c49727787e28320cbdc
mjoly75 <britanicus@hotmail.com>
2019-05-16 11:59:38 -0400

    se deconnecter dans menu deroulant

--- includes/headerMembre.php
+++ includes/headerMembre.php
@@ -6,15 +6,10 @@
 <li class="nav-item dropdown">
 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Bonjour <?php echo $nom?></a>
 <div class="dropdown-menu">
-    <a class="dropdown-item" href="formulaireDemandeur.php">Créer annonce</a>
+    <a class="dropdown-item" href="formulaireDemandeur.php">Créer une annonce</a>
     <a class="dropdown-item" href="profilUtilisateurAnnonces.php">Mes annonces</a>
     <a class="dropdown-item" href="profilUtilisateurAnnonces.php">Mes messages</a>
     <a class="dropdown-item" href="profilUtilisateur.php">Profil</a>
+    <a class="dropdown-item" href="logout.php">Se déconnecter</a>
 </div>
 </li>
-
-<li class="nav-item">
-<a class="nav-link" href="logout.php">Se déconnecter</a>
-</li>
-
-          
