1258895aefe3d2c1ed241ee91427d25d1f9f3e1e
mjoly75 <britanicus@hotmail.com>
2019-05-15 21:50:13 -0400

    template de base

--- profilUtilisateurModifier.php
+++ profilUtilisateurModifier.php
@@ -19,56 +19,48 @@ global $connexion,$idSession;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <script type="text/javascript" src="vues/vues.js"></script>
-    <script type="text/javascript" src="requetes/requetes.js"></script>
     <title>Profil</title>
+
+    <?php include("includes/header-script.php"); ?>
+
   </head>
-  <body>
-  <!-- debut container -->
-  <div class="container pt-5">
-    
-      
-  
-    <?php include("includes/menu.php"); ?>
-    <div class="container pt">
+  <body id="page-top">
+
+  <?php include("includes/menu.php"); ?>
+
+  <!-- debut section -->
+  <section id="projects" class="projects-section bg-light">
+    <!-- debut container -->
+    <div class="container pt-5">
       <div class="row">
-      <div class='col-12'>
-      <h1>Mon profil</h1>
-      </div>
+        <div class='col-12'>
+          <h1>Mon profil</h1>
+        </div>
         
-<?php
-       
-        
-        //requete update
-        global $connexion,$idSession;
-        //echo $idSession;
-        $nom=$_POST['nom'];
-        $prenom=$_POST['prenom'];
-        $sexe=$_POST['sexe'];
-        $ville=$_POST['ville'];
-        $dateNaissance=$_POST['dateNaissance'];
-        $codePostale=$_POST['codePostale'];
-        $telephone=$_POST['telephone'];
-        
-
-        
-
-$requete="UPDATE utilisateur SET nom=?,prenom=?,sexe=?,ville=?,dateNaissance=?,codePostale=?,telephone=? WHERE idUser='$idSession'";
- $stmt=$connexion->prepare($requete);
-$stmt->execute(array($nom,$prenom,$sexe,$ville,$dateNaissance,$codePostale,$telephone));
-echo "Vos informations ont été bien modifié !";
-unset($connexion);
-unset($stmt);	
-?>
-      
+            <?php
+              //requete update
+              global $connexion,$idSession;
+              //echo $idSession;
+              $nom=$_POST['nom'];
+              $prenom=$_POST['prenom'];
+              $sexe=$_POST['sexe'];
+              $ville=$_POST['ville'];
+              $dateNaissance=$_POST['dateNaissance'];
+              $codePostale=$_POST['codePostale'];
+              $telephone=$_POST['telephone'];
+                      
+              $requete="UPDATE utilisateur SET nom=?,prenom=?,sexe=?,ville=?,dateNaissance=?,codePostale=?,telephone=? WHERE idUser='$idSession'";
+              $stmt=$connexion->prepare($requete);
+              $stmt->execute(array($nom,$prenom,$sexe,$ville,$dateNaissance,$codePostale,$telephone));
+              echo "Vos informations ont été bien modifié !";
+              unset($connexion);
+              unset($stmt);	
+            ?>
     </div>
- 
-</div>
-</div>
-<!-- fin container -->
+  </div>
+  <!-- fin container -->
+</section>
+
 
     <?php include("includes/footer.php"); ?>
     <?php include("includes/footer-script.php"); ?>
