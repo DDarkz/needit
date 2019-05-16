4a2caceec639ee2018900f4c36c3dec73f29f477
mjoly75 <britanicus@hotmail.com>
2019-05-15 21:40:19 -0400

    template de base

--- contact.php
+++ contact.php
@@ -108,98 +108,53 @@ Thank you for contacting us. We will be in touch with you very soon.
 
   <title>Contact</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <script type="text/javascript" src="vues/vues.js"></script>
-  <script type="text/javascript" src="requetes/requetes.js"></script>  
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/grayscale.css" rel="stylesheet">
+  <?php include("includes/header-script.php"); ?>
 
 </head>
 
 <body id="page-top">
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="index.php"><img src="images\logoGoFor.png" width="100"></a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" class="dropdown-item active" href="propos.php">Propos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="annonces.php">Annonces</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="contact.php">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="description.php">Comment ça marche</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php include("includes/menu.php"); ?>
 
-  <!-- Header -->
-  <header class="contact">
-    <div class="container d-flex h-100 align-items-center">
-      <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 text-uppercase  couleur ">Contactez nous</h1>
-        <!-- <h2 class="text-white-50 mx-auto mt-2 mb-5">Rendez Service et gagner des bénéfices<br>
-          <p class="text-white-50">aidez les gens et gagner de l'argent</p></h2>
-        <a href="annonces.php" class="btn btn-primary js-scroll-trigger">Commencez</a> -->
-      </div>
-    </div>
-  </header>
+  
 
-  <!-- About Section -->
-  <section id="about" class="about-section">
+   <!-- debut section -->
+   <section id="projects" class="projects-section bg-light">
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mx-auto">
-          <h2 class="text-white mb-4">GoFor !</h2>
-          <p class="text-white-50"></p>
+        <h1>Contactez-nous</h1>
+          <h2 class="mb-4">GoFor !</h2>
+
           <form name="contactform" method="post" action="contact.php"> 
           <div class="form-group row">
-            <label for="nom" class="text-white-50 col-sm-2 col-form-label">Nom</label>
+            <label for="nom" class="col-sm-2 col-form-label">Nom</label>
             <div class="col-sm-10">
               <input type="text" class="form-control" id="nom" name="nom" Required>
             </div>
            
           </div>
           <div class="form-group row">
-            <label for="prenom" class="text-white-50 col-sm-2 col-form-label">Prénom</label>
+            <label for="prenom" class="col-sm-2 col-form-label">Prénom</label>
             <div class="col-sm-10">
               <input type="text"  class="form-control" id="prenom" name="prenom"  Required >
             </div>
           </div>
           <div class="form-group row">
-            <label for="courriel" class="text-white-50 col-sm-2 col-form-label">Courriel</label>
+            <label for="courriel" class="col-sm-2 col-form-label">Courriel</label>
             <div class="col-sm-10">
               <input type="email" class="form-control" id="courriel" name="courriel" aria-describedby="emailHelp"  Required>
             </div> 
             
           </div>
           <div class="form-group row">
-            <label for="objet" class="text-white-50 col-sm-2 col-form-label">Objet</label>
+            <label for="objet" class="col-sm-2 col-form-label">Objet</label>
             <div class="col-sm-10">
               <input type="text"  class="form-control" id="objet" name="objet"  Required >
             </div>
           </div>
           <div class="form-group row">
-            <label for="sujet" class="text-white-50 col-sm-2 col-form-label">Sujet</label>
+            <label for="sujet" class="col-sm-2 col-form-label">Sujet</label>
             <div class="col-sm-10">
               <textarea rows="4" cols="50" id="sujet" name="sujet" class="form-control" ></textarea>
             </div>
@@ -219,102 +174,10 @@ Thank you for contacting us. We will be in touch with you very soon.
     </div>
   </section>
 
-  <!-- Projects Section -->
-  <section id="projects" class="projects-section bg-light">
-    <div class="container" id="annoncesIndex">
 
-
-
-    </div>
-  </section>
-<!-- 
-  Signup Section
-  <section id="signup" class="signup-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-lg-8 mx-auto text-center">
-
-          
-
-        </div>
-      </div>
-    </div>
-  </section> -->
-
-  <!-- Contact Section -->
-  <section class="contact-section bg-black">
-    <div class="container">
-
-      <div class="row">
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Adresse</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">9155 Rue St-Hubert, Montréal, QC H2M 1Y8</div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-envelope text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Email</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">
-                <a href="#">Gofor@gmail.com</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-mobile-alt text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Phone</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">+1 (555) 902-8832</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="social d-flex justify-content-center">
-        <a href="#" class="mx-2">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#" class="mx-2">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#" class="mx-2">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
-
-    </div>
-  </section>
-
-  <!-- Footer -->
-  <footer class="bg-black small text-center text-white-50">
-    <div class="container">
-      Copyright &copy; GoFor 2019
-    </div>
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/grayscale.min.js"></script>
-
+  
+    <?php include("includes/footer.php"); ?>
+    <?php include("includes/footer-script.php"); ?>
 </body>
 
 </html>
--- formulaireLogin.php
+++ formulaireLogin.php
@@ -53,20 +53,18 @@ if(isset($_POST["login"]))
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <script type="text/javascript" src="vues/vues.js"></script>
-    <script type="text/javascript" src="requetes/requetes.js"></script>
-
     <title>Se connecter</title>
+
+    <?php include("includes/header-script.php"); ?>
   </head>
   <body>
 
     <?php include("includes/menu.php"); ?>
    
+    <!-- debut section -->
+    <section id="projects" class="projects-section bg-light">
     <!-- debut container -->
-    <div class="container pt">
+    <div class="container pt-5">
       <?php
         if (isset($message)) {
           echo '<div class="alert alert-danger" role="alert">'.$message.'</div>';
@@ -87,10 +85,11 @@ if(isset($_POST["login"]))
           <p><a href="#"><br>Mot de passe oublié? </a></p>
         </form>
         <!-- fin formulaire -->
-
     </div>
     <!-- fin container -->
-
+    </section>
+    <!-- fin section -->
+    
     <?php include("includes/footer.php"); ?>
     <?php include("includes/footer-script.php"); ?>
   </body>
